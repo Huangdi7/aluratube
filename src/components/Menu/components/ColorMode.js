8bdd189f2b828d4d7b73498b660f7a27122465d9
Hu4ng7 <caueatelirio241@gmail.com>
2022-11-12 23:56:12 -0300

    atualizacao aula 3

---
+++ src/components/Menu/components/ColorMode.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export const  ColorModeContext = React.createContext({
+    mode: "",
+    setMode: () => {alert("você precisa me configurar primeiro!") },
+    toggleMode: () => {alert("você precisa me configurar primeiro!") }
+});
+
+export default function ColorModeProvider(props) {
+    const [mode, setMode] = React.useState(props.initialMode)
+
+    function toggleMode(){
+        if( mode === "light") setMode("dark");
+        if( mode === "dark") setMode("light");
+    }
+    return (
+        <ColorModeContext.Provider value={({mode: mode, setMode: setMode, toggleMode: toggleMode})}>
+            {props.children}
+        </ColorModeContext.Provider>
+    )
+}
